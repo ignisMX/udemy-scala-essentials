6810e35cd4c54822d366fb942f7679b6287a3640
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2025-05-23 01:54:18 +0000

    it adds values and variable types concepts

---
+++ src/main/scala/basics/ValuesVariablesTypes.scala
@@ -0,0 +1,19 @@
+package basics
+
+object ValuesVariablesTypes extends App {
+
+  //vals are immutable
+
+  val aInteger : Int = 42
+  val aString : String = "Hello scala"
+  val aBoolean: Boolean = true
+  val asShort: Short = 4613
+  val aLong : Long = 123456789L
+  val aFloat: Float = 2.0f
+  val aDouble : Double =3.14
+
+  //vars are mutable
+  var aVariable: Int = 4;
+  aVariable = 5
+
+}
