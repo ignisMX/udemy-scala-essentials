c6f1e65f240b86a896533cdbdc73cf8f84b3bf05
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2025-06-07 07:45:52 +0000

    it adds method notations concepts

---
+++ src/main/scala/oobasics/MethodNotations.scala
@@ -0,0 +1,55 @@
+package oobasics
+
+import scala.language.postfixOps
+
+object MethodNotations extends App {
+
+  class Person(val name: String, favoriteMovie: String) {
+
+    def likes(movie: String): Boolean = movie == this.favoriteMovie
+
+    def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+
+    def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+
+    def unary_! : String = s"${this.name} what the heck?"
+
+    def isAlive: Boolean = true
+
+    def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+  }
+
+  private val mary = new Person("Mary", "Inception")
+  println(mary.likes("Inception"))
+  println(mary likes "Inception")
+
+  private val tom = new Person("Tom", "Fight Club")
+  println(mary.hangOutWith(tom))
+  println(mary hangOutWith tom)
+  println(mary + tom)
+  println(mary.+(tom))
+
+  println(1 + 2)
+  println(1.+(2))
+
+  //All operators are methods
+  //Akka operators ! ?
+
+  //Prefix notation
+  val x = -1 // is equivalent to unary_-
+  val y = 1.unary_-
+  //Unary only works with - + ~ !
+
+  println(mary.unary_!)
+  println(!mary)
+
+  //postfix notation
+  println(mary.isAlive)
+  println(mary isAlive)
+
+  //apply
+  println(mary.apply())
+  println(mary()) //equivalent to mary.apply()
+
+
+}
