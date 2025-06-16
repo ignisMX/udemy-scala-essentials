6a2407dee8bb7774eb20e29d4dc022f589965b7a
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2025-06-16 18:56:58 +0000

    it adds exercises of exceptions

--- src/main/scala/oobasics/Exceptions.scala
+++ src/main/scala/oobasics/Exceptions.scala
@@ -35,9 +35,10 @@ object Exceptions extends App {
 
   // 3. how to define your own exceptions
   class MyException extends Exception
+
   val myException = new MyException
 
-  throw myException
+  // throw myException
 
   /*
     1. Crash your program with an OutOfMemoryError
@@ -53,4 +54,50 @@ object Exceptions extends App {
         - UnderflowException if subtract(x,y) exceeds Int.MIN_VALUE
         - MatchCalculationException for division by 0
    */
+
+  // OutOfMemory Error
+  // val array = Array.ofDim[Int](Int.MaxValue)
+
+  // StackOverflow Error
+  // def infiniteLoop: Int = 1 + infiniteLoop
+  // val noLimit = infiniteLoop
+
+  class OverflowException extends Exception
+
+  class UnderflowException extends Exception
+
+  class MatchCalculationException extends Exception
+
+  object PocketCalculator {
+    def add(numberOne: Int, numberTwo: Int): Int = {
+      val result = numberOne + numberTwo
+      if (numberOne > 0 && numberTwo > 0 && result < 0) throw new OverflowException
+      else if (numberOne < 0 && numberTwo < 0 && result > 0) throw new UnderflowException
+      result
+    }
+
+    def subtract(numberOne: Int, numberTwo: Int): Int = {
+      val result = numberOne - numberTwo
+      if (numberOne > 0 && numberTwo < 0 && result < 0) throw new OverflowException
+      else if (numberOne < 0 && numberTwo > 0 && result > 0) throw new UnderflowException
+      result
+    }
+
+    def multiply(numberOne: Int, numberTwo: Int):Int = {
+      val result = numberOne * numberTwo
+      if (numberOne > 0 && numberTwo > 0 && result < 0) throw new OverflowException
+      else if (numberOne < 0 && numberTwo < 0 && result < 0) throw new OverflowException
+      else if (numberOne > 0 && numberTwo < 0 && result > 0) throw new UnderflowException
+      else if (numberOne < 0 && numberTwo > 0 && result > 0) throw new UnderflowException
+      result
+    }
+
+    def divide(numberOne: Int, numberTwo: Int): Int = {
+      if (numberTwo == 0) throw new MatchCalculationException
+      else numberOne / numberTwo
+    }
+  }
+
+  // println(PocketCalculator.add(Int.MaxValue, 10))
+  // println(PocketCalculator.divide(10,0))
 }
