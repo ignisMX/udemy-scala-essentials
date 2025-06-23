e3e905a852a30275b5344851ea63ae6c3b02754a
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2025-06-23 01:44:44 +0000

    it adds map flatmap filter and for-comprehensions concepts

---
+++ src/main/scala/functional/MapFlatmapFilterFor.scala
@@ -0,0 +1,46 @@
+package functional
+
+object MapFlatmapFilterFor extends App {
+
+  val list = List(1, 2, 3)
+  println(list.head)
+  println(list.tail)
+
+  //map
+  println(list.map(_ + 1))
+  println(list.map(_ + " is a number"))
+
+  //filter
+  println(list.filter(_ % 2 == 0))
+
+  val toPair = (element: Int) => List(element, element + 1)
+  println(list.flatMap(toPair))
+
+  val numbers = List(1, 2, 3)
+  val chars = List('a', 'b', 'c')
+  val colors = List("black", "white")
+
+  //List("a1", "a2","a3")
+
+  val combination = numbers.flatMap(n => chars.map(c => "" + c + n))
+  println(combination)
+
+  val combination2 = numbers.flatMap(number => chars.flatMap(character => colors.map(s"$number$character-" + _)))
+  println(combination2)
+
+  //foreach
+  list.foreach(print)
+
+  //for-comprehensions
+  val forCombination = for {
+    number <- numbers
+    character <- chars
+    color <- colors
+  } yield s"$number$character-$color"
+
+  println(forCombination)
+
+  // syntax overload
+  list.map { x => x * 2 }
+
+}
